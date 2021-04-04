7d0d04710f2322226918adcb1ad7f50a79bcf3fe
Alan Claughan <git@claughan.com>
2021-04-04 13:59:30 +0200

    my first attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas
 from turtle import Turtle, Screen
 
+FONT = ('Arial', 'regular', 12)
+
 pd = pandas.read_csv("50_states.csv")
 print(pd.head())
 states = pd['state'].to_list()
@@ -11,13 +13,16 @@ scr.setup(width = 725, height = 491)
 scr.bgpic("blank_states_img.gif")
 guess = scr.textinput(title = "Name the US states", prompt = "Name a state").title()
 
-if guess in states:
-    state_data = pd['state'] == guess
-    print(state_data)
+sd = pd[pd['state'] == guess]
+if not sd.empty:
+    print(sd)
 
-bob = Turtle()
-bob.hideturtle()
-bob.penup()
+
+    bob = Turtle()
+    bob.hideturtle()
+    bob.penup()
+    bob.goto(sd['x'], sd['y'])
+    bob.write(sd['state'], align = "center", font = FONT)
 
 
 
